59fb4cb29e78e7045eca9a56753d79b2ca222c3f
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2018-11-18 08:56:15 +0530

    feat(test): run tests in browser

--- test/Weber/Event.js
+++ test/Weber/Event.js
@@ -18,12 +18,4 @@ exports.e2 = {
   altKey: true,
 };
 
-// Polyfill because Node doesn't have these things.
-function MouseEvent(name, properties) {
-  this.type = name;
-  Object.assign(this, properties);
-}
-function KeyboardEvent() {}
-global.window = { MouseEvent: MouseEvent, KeyboardEvent: KeyboardEvent };
-
 exports.e3 = new MouseEvent('click', { bubbles: true });
