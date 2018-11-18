e6285e9db9f6e0c487360fb05174e4acb1e2f38b
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2018-11-18 09:14:34 +0530

    feat(Weber.Window.isEventTargetWindow)

--- src/Weber/Window.purs
+++ src/Weber/Window.purs
@@ -1,5 +1,9 @@
 module Weber.Window where
 
+import Weber.EventTarget (class IsEventTarget)
+
 foreign import data Window :: Type
 
 foreign import window :: Window
+
+instance isEventTargetWindow :: IsEventTarget Window
